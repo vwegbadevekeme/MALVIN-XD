c2421cad5c0a70502732941f696b5915f87c2597
vwegbadevekeme <vwegbadebekeme@gmail.com>
2025-07-16 15:09:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID (keep secure)
+  SESSION_ID: process.env.SESSION_ID || "", malvin~VyEVjLwR#dPI7EIL-XqoSEVvbKF34T4PeHLAyNzxGh9oK_Mn4uMs// Your bot's session ID (keep secure)
   PREFIX: getConfig("PREFIX") || ".", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴍᴀʟᴠɪɴ-xᴅ", // Bot display name
